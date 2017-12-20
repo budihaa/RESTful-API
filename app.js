7befcb6eb7a3096c8e5a2bbc1461479206f70a22
Budi Haryono <budiharyono4@gmail.com>
2017-12-20 14:24:07 +0700

    add bodyParser and handling CORS

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const morgan = require('morgan');
+const bodyParser = require('body-parser');
 
 // Import routes files
 const productRoutes = require('./api/routes/products'); 
@@ -8,6 +9,21 @@ const orderRoutes = require('./api/routes/orders');
 
 // Use morgan to log on console what happen
 app.use(morgan('dev'));
+
+// Body Parser to read json data from user
+app.use(bodyParser.urlencoded({extended: false})); // to read encoded URL
+app.use(bodyParser.json());
+
+// Handling CORS
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With', 'Content-Type', 'Accept', 'Authorization');
+  if (req.method === 'OPTIONS') {
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PATH, PUT, DELETE');
+    return res.status(200).json({});
+  }
+  next();
+});
 
 // Set the routes
 app.use('/products', productRoutes);
@@ -30,6 +46,5 @@ app.use((error, req, res, next) => {
     }
   })
 })
-
 
 module.exports = app;
